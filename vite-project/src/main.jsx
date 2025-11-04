93ee3a5abeb315f96889b51002337b401c20f1be
Vaishnavi Vagh <varshavagh2410@gmail.com.com>
2025-11-04 17:14:50 +0530

    new update

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+
+
+//when we add more function and components this render in this main jsx folder as well.
+)
